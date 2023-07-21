b4989048a69edf8c4cc6d2b0988baa21b01286df
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-07-21 22:56:02 +0800

    Updated navbar, cover page, footer

--- scripts/global.js
+++ scripts/global.js
@@ -6,25 +6,27 @@ const navBar = () => {
 		</a>
 		<button class="navbar-toggler" type="button"  data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span> 
-				<i class="fas fa-bars" style="color: #e0d625;"></i>
+			<i class="fa-solid fa-circle-chevron-down fa-lg" style="color: #e0d625;"></i>
 			</span>
 		</button>
-		<nav class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav gap-1 text-center fw-bold">
-				<li class="nav-item">
-						<a href="#" class="nav-link text-uppercase home" aria-current="page">Home</a>
-				</li>
-				<li class="nav-item">
-						<a href="#" class="nav-link text-uppercase home" aria-current="page">Discover</a>
-				</li>
-				<li class="nav-item">
-						<a href="#about" class="nav-link text-uppercase">Browse</a>
-				</li>
-			</ul>	
-			<form action="">
+			<div class="navbar-buttons">
+				<nav class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav gap-1 text-center fw-bold">
+						<li class="nav-item">
+								<a href="./index.html" class="nav-link text-uppercase home" aria-current="page">Home</a>
+						</li>
+						<li class="nav-item">
+								<a href="#" class="nav-link text-uppercase discover">Discover</a>
+						</li>
+						<li class="nav-item">
+								<a href="#about" class="nav-link text-uppercase">Browse</a>
+						</li>
+					</ul>	
+				<form action="">
 				<input type="search" required>
 				<i class="fa fa-search"></i>
-			</form>	
+				</form>	
+			</div>
 		</nav>`;
 
 	
