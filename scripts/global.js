ec851efbcc33d1ea9e1901a7ee8a9f25e26d3fc4
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-07-21 21:07:44 +0800

    :lipstick: Updated Navbar for Desktop version

--- scripts/global.js
+++ scripts/global.js
@@ -1,7 +1,9 @@
 const navBar = () => {
 	const bodyContainer = document.getElementById('navbody')
 	bodyContainer.innerHTML = `
-	
+		<a href="#" class="logo1 navbar-brand ">
+		<img src="./photo/logo.png" alt="logo" width="160" height="40">
+		</a>
 		<button class="navbar-toggler" type="button"  data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span> 
 				<i class="fas fa-bars" style="color: #e0d625;"></i>
@@ -9,6 +11,9 @@ const navBar = () => {
 		</button>
 		<nav class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav gap-1 text-center fw-bold">
+				<li class="nav-item">
+						<a href="#" class="nav-link text-uppercase home" aria-current="page">Home</a>
+				</li>
 				<li class="nav-item">
 						<a href="#" class="nav-link text-uppercase home" aria-current="page">Discover</a>
 				</li>
