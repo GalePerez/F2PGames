85d1b5bbed44ad1b32236242267396ad2151677c
Tynneth Jhoel Fuentes <pheonix0014@gmail.com>
2023-07-29 08:57:21 +0800

    game info added in home

--- scripts/home.js
+++ scripts/home.js
@@ -1,44 +1,43 @@
 const fetchGames = () => {
-	const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
-			const options = {
-				method: 'GET',
-				headers: {
-					'X-RapidAPI-Key': '2ca2ec8f4dmsh51d5ffeca17b406p156ec8jsnca55603af315',
-					'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-				}
-			};
+  const url = "https://free-to-play-games-database.p.rapidapi.com/api/games";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "2ca2ec8f4dmsh51d5ffeca17b406p156ec8jsnca55603af315",
+      "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+    },
+  };
 
-			fetch (url,options) 
-					.then((res) => {
-						if (res.ok) {
-							return res.json();
-						} else {
-							console.log('error');
-						}
-					}).then (data => {
-						// discover content
-						data.forEach(element => {
-
-							if (element.id <= 18) {
-								const discoverImage = document.getElementById('game-container');
-								const discoverContainer = document.createElement('div')
-								discoverContainer.classList.add('game-content', 'card', 'text-bg-dark')
-								const gameImage = document.createElement('img');
-								gameImage.classList.add('card-img', 'rounded-4');
-								gameImage.setAttribute('src',`${element.thumbnail}`);
-								discoverImage.appendChild(discoverContainer);
-								discoverContainer.appendChild(gameImage);
-								gameImage.addEventListener('click', () => {
-									window.location.href = element.game_url;
-								})
-							}
-
-						});
-					});
+  fetch(url, options)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        console.log("error");
+      }
+    })
+    .then((data) => {
+      // discover content
+      data.forEach((element) => {
+        if (element.id <= 18) {
+          const discoverImage = document.getElementById("game-container");
+          const discoverContainer = document.createElement("div");
+          discoverContainer.classList.add(
+            "game-content",
+            "card",
+            "text-bg-dark"
+          );
+          const gameImage = document.createElement("img");
+          gameImage.classList.add("card-img", "rounded-4");
+          gameImage.setAttribute("src", `${element.thumbnail}`);
+          discoverImage.appendChild(discoverContainer);
+          discoverContainer.appendChild(gameImage);
+          gameImage.addEventListener("click", () => {
+            window.location.href = `./game-info.html?id=${elements.id}`;
+          });
+        }
+      });
+    });
 };
 
-
-
-
 fetchGames();
-
