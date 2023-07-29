7a70925738adbb98c970aff8968f981d3991856d
Tynneth Jhoel Fuentes <pheonix0014@gmail.com>
2023-07-29 08:35:25 +0800

    remove ALL from genre

--- scripts/gamelist.js
+++ scripts/gamelist.js
@@ -25,7 +25,7 @@ dropContainer.appendChild(dropDown);
 // game content
 
 const url = "https://free-to-play-games-database.p.rapidapi.com/api/games";
-const options = { 
+const options = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "2ca2ec8f4dmsh51d5ffeca17b406p156ec8jsnca55603af315",
@@ -55,7 +55,6 @@ fetch(url, options)
 // game genres
 
 const genres = [
-  "All",
   "MMO",
   "MMORPG",
   "Shooter",
